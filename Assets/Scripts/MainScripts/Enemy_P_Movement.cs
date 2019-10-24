6f56e1a3b91299bd99c62ed831d48a2e2ed9a71f
Kent Vuong <kentvuong88@gmail.com>
2019-10-24 15:16:55 +0800

    my changes

--- Assets/Scripts/MainScripts/Enemy_P_Movement.cs
+++ Assets/Scripts/MainScripts/Enemy_P_Movement.cs
@@ -56,8 +56,9 @@ public class Enemy_P_Movement : MonoBehaviour
     {
         if(timeBtwShots <= 0)
         {
+            timeBtwShots = startTimeBtwShots;
             //Fires a bullet and starts a countdown
-            //Instantiate(projectile, firePoint.position , firePoint.rotation);
+            Instantiate(projectile, firePoint.position , firePoint.rotation);
             timeBtwShots = startTimeBtwShots;
         }
         else
